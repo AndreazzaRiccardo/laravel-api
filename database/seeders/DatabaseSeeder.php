d7f4ddf076acad0e765abf47ff243653e2ad1b1e
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2024-02-08 19:04:12 +0100

    change: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypesTableSeeder::class,
             TechnologiesTableSeeder::class,
-            ProjectsTableSeeder::class,
         ]);
     }
 }
